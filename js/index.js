0778dd14c16084cd37f141349ca5c7c6f7bea48f
Joshua Michael Waggoner <rabbitfighter@cryptolab.net>
2016-03-10 15:03:03 -0700

    Formatted JS

--- js/index.js
+++ js/index.js
@@ -8,59 +8,59 @@
 // ----- Functions ----- //
 
 function getData() {
-	// Endpoint URL
-	var URL = 'http://54.200.61.193/clientInfoData.json'
-	// Make a new promise to fetch data
-	var promise = fetchJsonFromEndpoint(URL)
-	// Promise is fulfilled
-	promise.then(function(data) {
-		$('#app').html('')
-		
-		var dataItems = data.split('\n')
+    // Endpoint URL
+    var URL = 'http://54.200.61.193/clientInfoData.json'
+        // Make a new promise to fetch data
+    var promise = fetchJsonFromEndpoint(URL)
+        // Promise is fulfilled
+    promise.then(function(data) {
+            $('#app').html('')
 
-		dataItems.map(function(item, index) {
-			addDataItem(item, index)
-		})
-		console.log('Done...')
-	})
-	.catch(function(err) {
-		$('#app').html('')
-	    console.error(err.log())
-	    $('#app').append(err.node())
-	})
+            var dataItems = data.split('\n')
+
+            dataItems.map(function(item, index) {
+                addDataItem(item, index)
+            })
+            console.log('Done...')
+        })
+        .catch(function(err) {
+            $('#app').html('')
+            console.error(err.log())
+            $('#app').append(err.node())
+        })
 }
 
 function fetchJsonFromEndpoint(url) {
     return new Promise(function(resolve, reject) {
- 		$.ajax({ 
-		    type: 'GET', 
-		    url: url, 
-		    dataType: 'text', // server responds w/ invalid json 
-		    success: function(data) { 
-		        resolve(data)
-		    },
-		    error: function(xhr, options, err) {
-		        reject(new FetchError(xhr, err))
-		    }
-		})
+        $.ajax({
+            type: 'GET',
+            url: url,
+            dataType: 'text', // server responds w/ invalid json 
+            success: function(data) {
+                resolve(data)
+            },
+            error: function(xhr, options, err) {
+                reject(new FetchError(xhr, err))
+            }
+        })
     })
 }
 
 function addDataItem(item, index) {
-	var div = document.createElement('div')
-	var dataIndex = document.createElement('h4')
-	var dataId = document.createElement('h5')
-	var dataItem = document.createElement('p')
+    var div = document.createElement('div')
+    var dataIndex = document.createElement('h4')
+    var dataId = document.createElement('h5')
+    var dataItem = document.createElement('p')
 
-	dataIndex.innerHTML = "Data Item " + index
-	dataId.innerHTML = "$oid: " + ((item) ? JSON.parse(item)._id.$oid : 'No id for item')
-	dataItem.innerHTML = (item) ? item.toString() : "No data for item"
+    dataIndex.innerHTML = "Data Item " + index
+    dataId.innerHTML = "$oid: " + ((item) ? JSON.parse(item)._id.$oid : 'No id for item')
+    dataItem.innerHTML = (item) ? item.toString() : "No data for item"
 
-	div.appendChild(dataIndex)
-	div.appendChild(dataId)
-	div.appendChild(dataItem)
+    div.appendChild(dataIndex)
+    div.appendChild(dataId)
+    div.appendChild(dataItem)
 
-	$('#app').append(div)
+    $('#app').append(div)
 }
 
 // ----- Objects ----- //
@@ -70,28 +70,25 @@ function FetchError(xhr, err) {
     this.status = xhr.status
     this.response = xhr.responseText
     this.node = function() {
-    	// Add error to DOM
-	    var node = document.createElement('div')
-		node.className = 'error'
-		node.style.textAlign = 'center'
-		node.style.color = 'red'
-		node.innerHTML = this.response
-		return node
+        // Add error to DOM
+        var node = document.createElement('div')
+        node.className = 'error'
+        node.style.textAlign = 'center'
+        node.style.color = 'red'
+        node.innerHTML = this.response
+        return node
     }
     this.log = function() {
-    	return (
-    		"Error:" + this.error + "\n\n" +
-    		"Status: " + this.status + "\n\n" +
-    		"Response: " + this.response
-    	)
+        return (
+            "Error:" + this.error + "\n\n" +
+            "Status: " + this.status + "\n\n" +
+            "Response: " + this.response
+        )
     }
 }
 
 // ----- Script ----- //
 getData()
 setInterval(function() {
-	getData()
+    getData()
 }, 10000)
-
-
-
